c989bf631e3ce49167cc55551ce4f662db0637b1
joemerrillis <joemerrillis@gmail.com>
2025-03-31 00:48:49 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
 export default defineConfig(({ mode }) => ({
   server: {
     host: "0.0.0.0",
@@ -5,7 +10,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
